5aa10042657fe2ec2ea9d6fe663066403c98fd0f
Aniruddha Aranya A Dheu <aranyaaniruddha@gmail.com>
2026-04-07 01:25:33 +0600

    add: time complexity analysis codes

---
+++ Data Structure/big-O.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int i, n, count;
+
+    scanf("%d", &n);
+
+    count = 0;
+
+    for(i = 0; i < n; i++){
+        count = count + 1;
+    }
+    printf("n = %d, count = %d\n", n, count);
+
+    return 0;
+}
