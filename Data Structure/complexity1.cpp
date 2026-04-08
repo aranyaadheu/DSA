979158cc8b9593d9e42da97302c22cd99a7beae8
Aniruddha Aranya A Dheu <aranyaaniruddha@gmail.com>
2026-04-09 01:39:48 +0600

    add: complexity analysis

--- Data Structure/complexity1.cpp
+++ Data Structure/complexity1.cpp
@@ -3,18 +3,14 @@
 int main()
 {
     int i, j, n, count;
-
     scanf("%d", &n);
 
     count = 0;
-
     for(i = 0; i < n; i++){
-        for ( j = 0; j < n; j++)
-        {
+        for(j = 0; j < n; j++){
             count = count + 1;
         }
     }
- 
     printf("n = %d, count = %d\n", n, count);
 
     return 0;
